ef49ecb4566dffa7460ddfe227009785ff56060f
cooc7565 <cooc7565@colorado.edu>
2016-04-27 21:51:13 -0600

    Create SongTree.h

---
+++ SongTree.h
@@ -0,0 +1,56 @@
+#ifndef SONGTREE_H
+#define SONGTREE_H
+#include <string>
+
+struct SongNode{
+    std::string artist;
+    std::string title;
+    int year;
+    std::string album;
+    std::string genre;
+    SongNode *parent;
+    SongNode *leftChild;
+    SongNode *rightChild;
+
+    SongNode(){};
+
+    SongNode(std::string in_artist, std::string in_title, int in_year, std::string in_album, std::string genre)
+    {
+        artist = in_ranking;
+        title = in_title;
+        year = in_year;
+        album = in_album;
+        genre = in_genre;
+        parent = NULL;
+        leftChild = NULL;
+        rightChild = NULL;
+    }
+
+};
+
+class SongTree
+{
+
+    public:
+        SongTree();
+        ~SongTree();
+        void printSongInventory();
+        int countSongNodes();
+        void deleteSongNode(std::string title);
+        void addSongNode(std::string artist, std::string title, int releaseYear, std::string album, std::string genre);
+        void findSong(std::string title);
+        void printGenre(std::string genre);
+        void printDecade(int year);
+
+    protected:
+
+    private:
+        void DeleteAll(SongNode * node); //use this for the post-order traversal deletion of the tree
+        void printSongInventory(SongNode * node);
+        void countSongNodes(SongNode *node, int *c);
+        SongNode* search(std::string title);
+        SongNode* treeMinimum(SongNode *node);
+        SongNode *root;
+};
+
+#endif // SONGTREE_H
